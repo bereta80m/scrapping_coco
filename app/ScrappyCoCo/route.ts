9e24ababdefd9e27b249fe7fb4ff63936c246dc8
unknown <bereta80m@gmail.com>
2024-02-29 09:32:43 -0400

    2 commit

--- app/ScrappyCoCo/route.ts
+++ app/ScrappyCoCo/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
-import puppeteer from "puppeteer-core";
+//import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer";
 
 const Auth = "";
 
