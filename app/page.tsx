e734d76a81a8029440f1fa0b24865a7dcd348607
unknown <bereta80m@gmail.com>
2024-02-29 09:50:15 -0400

    4 commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const res = await fetch(`http://localhost:3000/Scrapp`,{
-    headers:{"Content-Type": "application/json"}
-  })
-  const result = await res.json()
-  console.log(result)
+
+  const HandleFetching = async()=>{
+    const res = await fetch(`http://localhost:3000/Scrapp`,{
+      headers:{"Content-Type": "application/json"}
+    })
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      Home
+     <form action={HandleFetching}>
+          <button>Execute</button>
+     </form>
     </main>
   );
 }
